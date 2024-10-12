548a601b6b84ea5062e95f295d2788f18d9fc03a
Fadi Tawfig <ftawfig@gmail.com>
2024-10-12 16:28:47 -0400

    updating google clients to use .env service account

--- pages/api/create-firewall-rule.js
+++ pages/api/create-firewall-rule.js
@@ -58,10 +58,23 @@ export async function createRules(id, ip1, ip2, res) {
     }
 }
 
+const serviceAccount = {
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+  }
+
 export async function createFirewallRule(id, ranges, res, ipType) {
     const projectId = process.env.PROJECT;
-    const firewallsClient = new compute.FirewallsClient();
-    const operationsClient = new compute.GlobalOperationsClient();  
+    const firewallsClient = new compute.FirewallsClient({ credentials: serviceAccount });
+    const operationsClient = new compute.GlobalOperationsClient({ credentials: serviceAccount });  
 
     console.log(`ranges.length: ${ranges.length}`)
 
--- pages/api/server-status.js
+++ pages/api/server-status.js
@@ -1,26 +1,39 @@
 
 export default function handler(req, res) {
-    if (req.method === 'GET') {
-      //Handle HTTP POST request
-      return start_server();
-    } else {
-      //Handle any other HTTP method
-      return res.status(405).end(`Method ${req.method} Not Allowed`)
-    }
+  if (req.method === 'GET') {
+    //Handle HTTP POST request
+    return start_server();
+  } else {
+    //Handle any other HTTP method
+    return res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
 
   function server_status() {
     const instance = process.env.INSTANCE;
     const project = process.env.PROJECT;
     const zone = process.env.ZONE;
-    
+
     console.log('start_server() begin');
 
     //Imports the Compute library
-    const {InstancesClient} = require('@google-cloud/compute').v1;
-    
+    const { InstancesClient } = require('@google-cloud/compute').v1;
+
     //Instantiates a client
-    const computeClient = new InstancesClient();
-    
+    const serviceAccount = {
+      type: process.env.TYPE,
+      project_id: process.env.PROJECT_ID,
+      private_key_id: process.env.PRIVATE_KEY_ID,
+      private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: process.env.CLIENT_EMAIL,
+      client_id: process.env.CLIENT_ID,
+      auth_uri: process.env.AUTH_URI,
+      token_uri: process.env.TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.CLIENT_X509_CERT_URL
+    }
+
+    const computeClient = new InstancesClient({ credentials: serviceAccount });
+
     async function callGet() {
       //Construct request
       const request = {
@@ -28,7 +41,7 @@ export default function handler(req, res) {
         project,
         zone,
       };
-    
+
       //Run request
       const response = await computeClient.get(request);
 
